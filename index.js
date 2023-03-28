d0900497199ed4f2330ee80fb8121571bfdfca60
Jon Chen <jchen2190@gmail.com>
2023-03-28 17:54:50 -0400

    fix bug in express static

--- index.js
+++ index.js
@@ -11,14 +11,13 @@ const path = require("path");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(logger("dev"));
 
 const indexRouter = require("./routes/indexRouter");
-
 app.use("/", indexRouter);
 
 const PORT = 3000;
